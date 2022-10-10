9316ea4997d658d78080bc65e7856b65a74e8161
CharlyesRodrigues <charlyessouza@gmail.com>
2022-10-10 20:50:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -20,5 +20,22 @@ if(verifyExistsFile){
 // Segue exemplo => <Buffer 5b 0d 0a 20 20 20 20 7b 0d 0a 22 6e 61 6d 65 22 3a 20 22 4a 6f 73 c3 a9 22 0d 0a 20 20 7d 2c 0d 0a 0d 0a 7b 0d 0a 22 6e 61 6d 65 22 3a 20 22 4c 75 63 ... 9 more bytes>
 
 
-const userList= fs.readFileSync("D:/Nova pasta/Aulas Full Stack/18º WEB 3.0/Node/Node/Node.js/users.json" , {encoding : "utf-8"});
-console.log(userList);
+const userList = fs.readFileSync("D:/Nova pasta/Aulas Full Stack/18º WEB 3.0/Node/Node/Node.js/users.json" , {encoding : "utf-8"});
+/*console.log(userList); ao executar essa linha de comando ele mostra no terminal as informações abaixo. Como resoler ?
+[
+    {
+"name": "José"
+  },
+
+{
+"name": "Lucas"
+}
+]*/
+
+
+const parse= JSON.parse(userList);
+parse.map(user=> console.log(user.name)); 
+/* Agora sim ficou bem mais apresentável 
+José
+Lucas
+*/
