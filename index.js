6f399897845b9c1c986fda48c2d818b3dd8536fe
CharlyesRodrigues <charlyessouza@gmail.com>
2022-10-10 20:05:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const fs = requere("fs");
+const fs = require("fs");
+
+fs.writeFileSync("D:/Nova pasta/Aulas Full Stack/18º WEB 3.0/Node/Node/Node.js/ola.txt ", "Olá Mundo ! ");
+
+// Criando uma variável(verifyExistsFile) para receber um método(existsSync) que verifica a existência de um arquivo
+const verifyExistsFile = fs.existsSync("D:/Nova pasta/Aulas Full Stack/18º WEB 3.0/Node/Node/Node.js/ola.txt ");
+
+if(verifyExistsFile){
+
+    console.log("Existe")
+}else{
+
+    console.log("Não existe")
+}
