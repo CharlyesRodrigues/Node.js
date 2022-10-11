39eb9e93896d1e7a9c76c26da8a3f3147bba98b1
CharlyesRodrigues <charlyessouza@gmail.com>
2022-10-10 21:36:43 -0300

    Create index.js

---
+++ Criando_Servidor_HTTP/index.js
@@ -0,0 +1,20 @@
+const http =  require("http");
+
+const hostname = "localhost";
+
+const server = http.createServer((request, response)=>{
+
+response.statusCode = 200;
+response.setHeader("Content-type", "text/plain");
+response.end("Hello Word");
+})
+
+server.listen(3333, hostname, ()=>{
+
+console.log("Server running in localhost: 3333")
+
+
+})
+
+
+
