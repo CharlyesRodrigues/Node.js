55f5907525cb7dfb2eb1382fd13da326d255f6db
CharlyesRodrigues <charlyessouza@gmail.com>
2022-10-20 22:34:08 -0300

    Update index.js

--- Criando_Servidor_HTTP/index.js
+++ Criando_Servidor_HTTP/index.js
@@ -16,5 +16,26 @@ console.log("Server running in localhost: 3333")
 
 })*/
 
+import  express  from "express";
+import fs from "fs";
+import https from "https";
+import cors from "cors";
 
 
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.get("/", (req,res)=>{
+
+res.json({message: "Hello Word"});
+
+})
+app.listen(3000, () => console.log("api rodando...."));
+
+https.createServer({
+
+cert: fs.readdirSync("D:/Nova pasta/Aulas Full Stack/18º WEB 3.0/Node/Node/Node.js/Criando_Servidor_HTTP/certificate.crt"),
+key:  fs.readdirSync("D:\Nova pasta\Aulas Full Stack\18º WEB 3.0\Node\Node\Node.js\Criando_Servidor_HTTP\Key.key")
+
+}, app)
+.listen(3001, () => console.log("rodando em https"))
